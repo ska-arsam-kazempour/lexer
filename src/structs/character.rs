b9ef2d406cbc8dd185d075edac8f9f48419ff965
Arsam Kazempour <ska.arsam.kazempour@gmail.com>
2022-03-19 15:00:09 +0300

    1.

---
+++ src/structs/character.rs
@@ -0,0 +1,16 @@
+pub struct Character<'a> {
+    pub character_value: char,
+    pub character_type: &'a str
+}
+
+impl<'a> std::fmt::Display for Character<'a> {
+    fn fmt<'b>(&self, formatter: &mut std::fmt::Formatter<'b>) -> std::fmt::Result {
+        return write!(formatter, "CHARACTER: {} = \"{}\"", self.character_type, self.character_value);
+    }
+}
+
+impl<'a> std::fmt::Debug for Character<'a> {
+    fn fmt<'b>(&self, formatter: &mut std::fmt::Formatter<'b>) -> std::fmt::Result {
+        return write!(formatter, "CHARACTER: {} = \"{}\"", self.character_type, self.character_value);
+    }
+}
